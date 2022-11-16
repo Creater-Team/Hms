4fef9b4250d6d24782a232dd3b129453ac6362b3
Smuuse2 <Smuuse09@gmail.com>
2022-11-17 00:08:29 +0300

    first commit

--- Controllers/DoctorControlers.js
+++ Controllers/DoctorControlers.js
@@ -7,15 +7,42 @@ let doctors  = [{
     shift : "Morning",
 }]
 
-const AllDoctors = (req,res,next)=>{
+const getAllDoctors = (req,res,next)=>{
 
     res.json({
        status : "success",
         doctors,
-    })
+    });
+};
+
+// sxp halkan ila Hub error ayaa jira
+const createDoctor = (req,res, next)=> {
+
+    const {Name,Title,shift} = req.body;
+const findDoctor = doctors.find((doctors)=> doctors.id === Number(id));
+if (findDoctor?.id){
+
+    if(!Name || !Title || !shift){
+
+        res.status(403).json({
+            status: 'error',
+            message : 'please provide body date',
+        });
+    }else{
+
+        findDoctor.Name = Name;
+        findDoctor.Title = Title;
+        findDoctor.shift = shift;
+
+        res.jsom({
+            status: 'success',
+            message: 'Update sucessfully',
+            findDoctor,
+        });
+    }
+}
 }
 
 module.exports = {
-
-AllDoctors,
+getAllDoctors
 }
--- routes/doctorsRouter.js
+++ routes/doctorsRouter.js
@@ -3,6 +3,12 @@ const Exports = require('express');
 const router = Express.Router();
 
 
+// intan ayaan ku daray  
+const doctorController = require('../Controllers/DoctorControlers')
+
+// GET ALL WAXAAN KALA IMI DOCTOR CONTROLL
+router.get('./', doctorController.getAllDoctors)
+
 
 
 module.exports=router;
